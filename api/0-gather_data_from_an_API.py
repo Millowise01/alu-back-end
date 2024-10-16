7f737c00f64f4125175788b47135455e70b39219
Millowise01 <l.carew@alustudent.com>
2024-10-16 17:52:31 +0000

    gather data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-""" Library to gather data from an API """
+"""Script to gather TODO list progress of an employee from a REST API"""
 
 import requests
 import sys
 
-""" Function to gather data from an API """
+def get_employee_todo_progress(employee_id):
+    """Fetch and display TODO progress for a given employee ID."""
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-
-    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
-    todo = todo.format(employee_id)
-
-    user_info = requests.request("GET", url).json()
-    todo_info = requests.request("GET", todo).json()
+    user_info = requests.get(url).json()
+    todo_info = requests.get(todo_url).json()
 
     employee_name = user_info.get("name")
-    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
+    total_tasks = list(filter(lambda x: x.get("completed") is True, todo_info))
     task_com = len(total_tasks)
     total_task_done = len(todo_info)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-          task_com, total_task_done))
+    print(f"Employee {employee_name} is done with tasks({task_com}/{total_task_done}):")
 
-    [print("\t {}".format(task.get("title"))) for task in total_tasks]
+    [print(f"\t {task.get('title')}") for task in total_tasks]
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Please provide a valid integer for the employee ID.")
+        sys.exit(1)
